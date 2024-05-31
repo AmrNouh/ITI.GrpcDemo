0a4e070ded8aba9ae465f7e1bbc21d616325d01e
Amr M. Nouh <a.nouh@qoad.com>
2024-05-31 14:34:07 +0300

    Updated gRPC client and protocol definitions
    Updated the `ITI.GrpcDemo.Client.csproj` file to include necessary packages for a gRPC client application and set the target framework to .NET 8.0. Modified `Program.cs` to create a gRPC channel, instantiate a `TrackingServiceClient`, and send a `TrackingMessage` to the server. Defined the protocol for the gRPC service in `TrackingService.proto`, including the `TrackingService`, `TrackingMessage`, `Location`, `Sensor`, and `TrackingMessageResponse` types.

---
+++ ITI.GrpcDemo.Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Net.Client;
+using ITI.GrpcDemo.Client.Protos;
+using static ITI.GrpcDemo.Client.Protos.TrackingSerivce;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7053");
+var client = new TrackingSerivceClient(channel);
+
+var request = new TrackingMessage
+{
+    DeviceId = 1,
+    DateTime = Timestamp.FromDateTime(DateTime.UtcNow),
+    Message = "Hello from client",
+    Location = new Location
+    {
+        Latitude = 30.0444f,
+        Longitude = 31.2357f
+    },
+};
+request.Sensors.Add(new Sensor { Name = "Sensor1", Value = 1000 });
+
+var response = await client.ReviceMessageAsync(request);
+
+Console.WriteLine($"Response: {response.Status}");
