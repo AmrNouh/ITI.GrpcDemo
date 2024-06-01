ab983eb4fd1248a0e6958a7ebe53f73ea098071e
Amr M. Nouh <a.nouh@qoad.com>
2024-06-01 17:08:13 +0300

    Implemented KeepAlive and Subscribe methods in TrackingService
    In `Program.cs`, client creation for `TrackingService` has been modified to use the service's fully qualified name. Added `SendMessage` and `KeepAlive` methods. `SendMessage` sends a `TrackingMessage` to the server and handles `RpcException`. `KeepAlive` sends a stream of `PulseMessage` to the server.
    
    In `TrackingService.proto`, added `KeepAlive` and `Subscribe` methods to `TrackingService`. Also added `PulseMessage`, `MachineStatus` enum, `SubscribeMessage`, and `Nofication` message.
    
    In `MessageTrackingService.cs`, implemented `KeepAlive` and `Subscribe` methods. `KeepAlive` logs each `PulseMessage` received from the client. `Subscribe` sends a stream of `Nofication` messages to the client. Added a check in `ReviceMessage` method to ensure `DateTime` field of `TrackingMessage` is not null, throwing an `RpcException` with `InvalidArgument` status if it is.

--- ITI.GrpcDemo.Client/Program.cs
+++ ITI.GrpcDemo.Client/Program.cs
@@ -1,24 +1,71 @@
 ﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
 using Grpc.Net.Client;
 using ITI.GrpcDemo.Client.Protos;
-using static ITI.GrpcDemo.Client.Protos.TrackingSerivce;
 
 var channel = GrpcChannel.ForAddress("https://localhost:7053");
-var client = new TrackingSerivceClient(channel);
+var client = new TrackingSerivce.TrackingSerivceClient(channel);
+var rand = new Random();
 
-var request = new TrackingMessage
+await SendMessage(client);
+
+//await KeepAlive(client);
+
+//var call = client.Subscribe(new SubscribeMessage { DeviceId = 1 });
+
+//while (await call.ResponseStream.MoveNext(CancellationToken.None))
+//{
+//    var message = call.ResponseStream.Current;
+//    Console.WriteLine($"Message: {message.Message} at {message.DateTime}");
+//}
+
+
+static async Task SendMessage(TrackingSerivce.TrackingSerivceClient client)
 {
-    DeviceId = 1,
-    DateTime = Timestamp.FromDateTime(DateTime.UtcNow),
-    Message = "Hello from client",
-    Location = new Location
+    try
     {
-        Latitude = 30.0444f,
-        Longitude = 31.2357f
-    },
-};
-request.Sensors.Add(new Sensor { Name = "Sensor1", Value = 1000 });
+        var request = new TrackingMessage
+        {
+            DeviceId = 1,
+            //DateTime = Timestamp.FromDateTime(DateTime.UtcNow),
+            Message = "Hello from client",
+            Location = new Location
+            {
+                Latitude = 30.0444f,
+                Longitude = 31.2357f
+            },
+        };
+        request.Sensors.Add(new Sensor { Name = "Sensor1", Value = 1000 });
 
-var response = await client.ReviceMessageAsync(request);
+        var response = await client.ReviceMessageAsync(request);
 
-Console.WriteLine($"Response: {response.Status}");
+        Console.WriteLine($"Response: {response.Status}");
+    }
+    catch (RpcException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    
+}
+
+static async Task KeepAlive(TrackingSerivce.TrackingSerivceClient client)
+{
+    var call = client.KeepAlive();
+
+    foreach (var i in Enumerable.Range(1, 10))
+    {
+        await call.RequestStream.WriteAsync(new PulseMessage
+        {
+            DeviceId = i,
+            SentTime = Timestamp.FromDateTime(DateTime.UtcNow),
+            Status = MachineStatus.Start,
+            Details = new PulseMessage.Types.AdditionalDetails()
+            {
+                Title = "Title",
+                Description = "Description",
+            }
+        });
+        await Task.Delay(1000);
+    }
+    await call.RequestStream.CompleteAsync();
+}
--- ITI.GrpcDemo.Server/Services/MessageTrackingService.cs
+++ ITI.GrpcDemo.Server/Services/MessageTrackingService.cs
@@ -1,4 +1,5 @@
-﻿using Grpc.Core;
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
 using ITI.GrpcDemo.Server.Protos;
 using static ITI.GrpcDemo.Server.Protos.TrackingSerivce;
 
@@ -15,12 +16,60 @@ namespace ITI.GrpcDemo.Server.Services
 
         public override async Task<TrackingMessageResponse> ReviceMessage(TrackingMessage request, ServerCallContext context)
         {
+            if (request.DateTime is null)
+            {
+                Metadata entries = new Metadata
+                {
+                    { "DateTimeValue", "DateTime is required" }
+                };
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "DateTime is required"), entries);
+                
+            }
             _logger.LogInformation($"Message received from deviceId: {request.DeviceId} at location : ({request.Location.Latitude}, {request.Location.Longitude}) at {request.DateTime.ToDateTime()}");
 
             return await Task.FromResult(new TrackingMessageResponse
             {
                 Status = true
             });
+        }
+
+        public override async Task<Empty> KeepAlive(IAsyncStreamReader<PulseMessage> requestStream, ServerCallContext context)
+        {
+            //while (await requestStream.MoveNext())
+            //{
+            //    var request = requestStream.Current;
+            //    _logger.LogInformation($"Keep alive received from deviceId: {request.DeviceId} with status: {request.Status} at {request.SentTime.ToDateTime()} Additional Details: {request.Details.Title}");
+            //}
+            //while(!context.CancellationToken.IsCancellationRequested)
+            //{
+            //}
+
+            await foreach (var request in requestStream.ReadAllAsync())
+            {
+                _logger.LogInformation($"Keep alive received from deviceId: {request.DeviceId} with status: {request.Status} at {request.SentTime.ToDateTime()} Additional Details: {request.Details.Title}");
+            }
+
+            return await Task.FromResult(new Empty());
+        }
+
+        public override Task Subscribe(SubscribeMessage request, IServerStreamWriter<Nofication> responseStream, ServerCallContext context)
+        {
+            while(!context.CancellationToken.IsCancellationRequested)
+            {
+                Task.Run(async () =>
+                {
+                    var notification = new Nofication
+                    {
+                        Message = "Hello from server",
+                        DateTime = Timestamp.FromDateTime(DateTime.UtcNow)
+                    };
+
+                    await responseStream.WriteAsync(notification);
+                    await Task.Delay(1000);
+                });
+            }
+
+            return Task.CompletedTask;
         }
     }
 }
